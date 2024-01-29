35b147593d674f7ab3bb0a0577dae6bdacb5206a
Eadweard <804875728@qq.com>
2024-01-29 16:25:34 +0800

    command命令

--- cli.ts
+++ cli.ts
@@ -3,9 +3,11 @@ import path from "node:path"
 
 const version = require('./package.json').version
 
-const cli = cac('auno-cli').version(version).help()
+const cli = cac('auno').version(version).help()
 
-cli.option('--test <test>', 'show version')
+cli.command('create <project>', 'start create new project').action((project, options) => {
+  console.log(project, options)
+})
 
 const parsed = cli.parse()
 console.log(parsed)
