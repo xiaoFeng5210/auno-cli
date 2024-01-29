e16f73c17abcf147d8b1fbf9cb641a57ed54ed32
Eadweard <804875728@qq.com>
2024-01-29 16:55:21 +0800

    cac加入--template

--- cli.ts
+++ cli.ts
@@ -5,9 +5,11 @@ const version = require('./package.json').version
 
 const cli = cac('auno').version(version).help()
 
-cli.command('create <project>', 'start create new project').action((project, options) => {
-  console.log(project, options)
-})
+cli.command('create <project>', 'start create new project')
+  .option('--template <template>', 'choose a template', {default: 'node'})
+  .action((project, options) => {
+    console.log(project, options)
+  })
 
 const parsed = cli.parse()
 console.log(parsed)
