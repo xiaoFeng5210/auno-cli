b670d8b8dfd8bf832c73e696a0b8c9230ec9c541
Eadweard <804875728@qq.com>
2024-01-29 15:15:42 +0800

    feature: index文件生成完毕

--- src/createIndexFile.ts
+++ src/createIndexFile.ts
@@ -1,9 +1,7 @@
 import fs from 'node:fs/promises'
 
 export async function createIndexFile(dir: string) {
-  const fileContetnt = `
-  // Welcome to auno
-  `
+  const fileContetnt = `// Welcome to auno`
   await fs.writeFile(`${dir}/index.ts`, fileContetnt)
 
 }
