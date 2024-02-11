c08bd0ff45f826a365e8a421dc27e8cbd234a953
Eadweard <804875728@qq.com>
2024-02-11 13:37:47 +0800

    refactor: delete old code

--- auno.ts
+++ auno.ts
@@ -4,31 +4,9 @@ import { exec, execFile, spawn } from 'node:child_process';
 import path, { dirname } from 'node:path';
 import { renderTemplate } from './src/utils/renderTemplate'
 
+// test
 async function main() {
   renderTemplate('ts-node', 'test-render')
 }
 
-main()
-
-export async function createNodeProject(dir: string) {
-  const rootDir = dir ? dir : getTemplateDir()
-  if (fs.existsSync(rootDir)) {
-    // 删除文件夹
-    fs.rmSync(rootDir, { recursive: true, force: true });
-  }
-  fs.mkdirSync(rootDir)
-  fs.mkdirSync(`${rootDir}/src`)
-  fs.mkdirSync(`${rootDir}/assets`)
-  await createBase(rootDir)
-}
-
-async function createBase(dir: string) {
-  await createIndexFile(dir)
-  await createTsconfig(dir)
-  await createPackageFile(dir)
-  createTSup(dir)
-}
-
-function getTemplateDir() {
-  return 'auno-cli'
-}
+// main()
